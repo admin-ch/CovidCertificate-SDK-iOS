b1c2dc4975ab00d6aa1613425d4ab11c1ca5149a
Patrick Amrein <amrein@ubique.ch>
2021-06-11 13:27:08 +0200

    make national rules codable

--- Sources/CovidCertificateSDK/TrustList/NationalRulesListUpdate.swift
+++ Sources/CovidCertificateSDK/TrustList/NationalRulesListUpdate.swift
@@ -10,6 +10,9 @@
  */
 
 import Foundation
+import JSON
+
+
 
 class NationalRulesListUpdate: TrustListUpdate {
     // MARK: - Session
@@ -37,17 +40,38 @@ class NationalRulesListUpdate: TrustListUpdate {
             outcome = result
             semaphore.signal()
         }
-
+       
         semaphore.wait()
 
-        guard let result = try? outcome.get() else {
+        guard var result = try? outcome.get() else {
             return .NETWORK_PARSE_ERROR
         }
+        
+        let jwtString = String(data: d, encoding: .utf8)!
+        let components = jwtString.components(separatedBy: ".")
+        guard components.count == 2 || components.count == 3,
+              let claimsData = Data.data(base64urlEncoded: components[1])
+        else {
+            return .NETWORK_PARSE_ERROR
+        }
+        result.requestData = claimsData
         _ = trustStorage.updateNationalRules(result)
         return nil
     }
 
     override internal func isListStillValid() -> Bool {
         return trustStorage.nationalRulesListIsStillValid()
+    }
+}
+
+extension Data {
+    public static func data(base64urlEncoded: String) -> Data? {
+        let paddingLength = 4 - base64urlEncoded.count % 4
+        let padding = (paddingLength < 4) ? String(repeating: "=", count: paddingLength) : ""
+        let base64EncodedString = base64urlEncoded
+            .replacingOccurrences(of: "-", with: "+")
+            .replacingOccurrences(of: "_", with: "/")
+            + padding
+        return Data(base64Encoded: base64EncodedString)
     }
 }
