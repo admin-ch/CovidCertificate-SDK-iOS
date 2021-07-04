b8d4cae12c2fca4a51b63abf96a4b11291a40be6
Stefan Mitterrutzner <mitterrutzner@ubique.ch>
2021-07-04 23:14:16 +0200

    swiftformat

--- Tests/CovidCertificateSDKTests/TestTrustListManager.swift
+++ Tests/CovidCertificateSDKTests/TestTrustListManager.swift
@@ -93,7 +93,7 @@ class TestTrustStorage: TrustStorageProtocol {
 
     // MARK: - Active Certificates
 
-    func activeCertificatePublicKeys(useFilters: [String?]) -> [TrustListPublicKey] {
+    func activeCertificatePublicKeys(useFilters _: [String?]) -> [TrustListPublicKey] {
         return publicKeys
     }
 
