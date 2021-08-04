f7ca5f48425c4e4aceaacac05218a6fa28adcc89
Stefan Mitterrutzner <mitterrutzner@ubique.ch>
2021-08-04 14:26:47 +0200

    fixes bundle loader

--- Sources/CovidCertificateSDK/Helpers/Bundle+Loader.swift
+++ Sources/CovidCertificateSDK/Helpers/Bundle+Loader.swift
@@ -15,25 +15,15 @@ private class BundleFinder {}
 extension Foundation.Bundle {
     /// Returns the resource bundle associated with the current Swift module.
     static var module: Bundle = {
-        let bundleName = "CovidCertificateSDK_CovidCertificateSDK"
+        let myBundle = Bundle(for: BundleFinder.self)
 
-        let candidates = [
-            // Bundle should be present here when the package is linked into an App.
-            Bundle.main.resourceURL,
+        guard let resourceBundleURL = myBundle.url(
+            forResource: "CovidCertificateSDK", withExtension: "bundle")
+        else { fatalError("CovidCertificateSDK.bundle not found!") }
 
-            // Bundle should be present here when the package is linked into a framework.
-            Bundle(for: BundleFinder.self).resourceURL,
+        guard let resourceBundle = Bundle(url: resourceBundleURL)
+        else { fatalError("Cannot access CovidCertificateSDK.bundle!") }
 
-            // For command-line tools.
-            Bundle.main.bundleURL,
-        ]
-
-        for candidate in candidates {
-            let bundlePath = candidate?.appendingPathComponent(bundleName + ".bundle")
-            if let bundle = bundlePath.flatMap(Bundle.init(url:)) {
-                return bundle
-            }
-        }
-        fatalError("unable to find bundle named \(bundleName)")
+        return resourceBundle
     }()
 }
